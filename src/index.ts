077583ad2096f32fac2cfc996aa893d7d1c9c7bc
aaaaninja <keshigomu71505@gmail.com>
2019-07-22 16:58:03 +0900

    :tada: 処理の雛形を作成

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as fs from "fs";
+
+import puppeteer from "puppeteer";
+
+(async () => {
+////////////////////////////////////////////////////////////////////////////////
+const urls = [ "https://github.com/GoogleChrome/puppeteer/blob/master/README.md"
+             , "https://github.com/GoogleChrome/puppeteer"
+             ];
+const browser = await puppeteer.launch({args: ['--no-sandbox','--disable-setuid-sandbox'], headless: false}); // debug用途にheadlessをfalseにしてgui表示させてる.
+const tabs    = await Promise.all([...Array(100)].map(() => browser.newPage())); // テキトーに100枚バっとtabを開いてる.
+await Promise.all(urls.map((url, idx) => tabs[idx].goto(url))); // すべて並列で進むが、次に行に行くのは一番遅い読み込みのやつに引っ張られる.
+await Promise.all(urls.map((url, idx) => tabs[idx].screenshot({path: `${idx}_oo.png`})));
+await browser.close();
+////////////////////////////////////////////////////////////////////////////////
+})();
